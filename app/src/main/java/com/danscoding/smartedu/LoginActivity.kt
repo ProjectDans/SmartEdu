219201877784203c8c9357b1caa78cfba3ae37ef
Daniel Celo <12113268>
2022-11-17 08:37:45 +0700

    Membuat halaman register

--- app/src/main/java/com/danscoding/smartedu/LoginActivity.kt
+++ app/src/main/java/com/danscoding/smartedu/LoginActivity.kt
@@ -1,11 +1,22 @@
 package com.danscoding.smartedu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var tvDaftar : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        tvDaftar = findViewById(R.id.tvdaftardisini)
+        tvDaftar.setOnClickListener {
+            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
